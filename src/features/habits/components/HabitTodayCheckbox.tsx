ce620610b54402493c05f05b39fc7cb0de947fd6
Zehn77 <zekhn777@gmail.com>
2025-08-01 11:25:22 +0500

    feat(progress): improve checkbox responsiveness using local state with debounce

--- src/features/habits/components/HabitTodayCheckbox.tsx
+++ src/features/habits/components/HabitTodayCheckbox.tsx
@@ -1,10 +1,12 @@
 import { Checkbox } from "@/shared/components/checkbox";
 import { Label } from "@/shared/components/label";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import type { HabitProgressEntry } from "@/features/progress/types";
 import { addProgress, deleteProgress } from "@/features/progress/slice";
 import { formatDateToISO } from "@/shared/lib/date";
 import { selectTodayProgressByHabitId } from "@/features/progress/progressSelectors";
+import { useDebouncedCallback } from "use-debounce";
 
 type HabitTodayCheckboxProps = {
   habitId: string;
@@ -12,24 +14,34 @@ type HabitTodayCheckboxProps = {
 
 function HabitTodayCheckbox({ habitId }: HabitTodayCheckboxProps) {
   const dispatch = useDispatch();
-
   const habitProgress = useSelector(selectTodayProgressByHabitId(habitId));
-  const checked: boolean = habitProgress?.status === "completed" || false;
+  const initiallyChecked = habitProgress?.status === "completed";
 
-  function handleCheckboxChange(value: boolean) {
-    if (!checked) {
+  const [isChecked, setIsChecked] = useState(initiallyChecked);
+
+  useEffect(() => {
+    setIsChecked(initiallyChecked);
+  }, [initiallyChecked]);
+
+  const debouncedToggleProgress = useDebouncedCallback((value: boolean) => {
+    if (value) {
       const newHabitProgress: HabitProgressEntry = {
         date: formatDateToISO(new Date()),
         habitId,
         updatedAt: new Date().toISOString(),
-        status: value ? "completed" : null,
+        status: "completed",
       };
       dispatch(addProgress(newHabitProgress));
-      return;
+    } else {
+      dispatch(deleteProgress(habitId));
     }
+    console.log("value changed");
+  }, 300);
 
-    dispatch(deleteProgress(habitId));
-  }
+  const handleCheckboxChange = (value: boolean) => {
+    setIsChecked(value);
+    debouncedToggleProgress(value);
+  };
 
   return (
     <Label
@@ -38,14 +50,14 @@ function HabitTodayCheckbox({ habitId }: HabitTodayCheckboxProps) {
     >
       <Checkbox
         id={habitId}
-        checked={checked}
+        checked={isChecked}
         onCheckedChange={handleCheckboxChange}
         className="border border-gray-300 data-[state=checked]:bg-green-500 data-[state=checked]:text-white data-[state=checked]:border-green-500"
         aria-label={`Mark habit as ${
-          checked ? "uncompleted" : "completed"
+          isChecked ? "uncompleted" : "completed"
         } for today`}
       />
-      {checked ? (
+      {isChecked ? (
         <span className="underline text-green-500">Completed</span>
       ) : (
         <span className="line-through">Uncompleted</span>
